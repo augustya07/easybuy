d9aa4f68ed7cd4e53c78aee4ee2543c6062bf678
Austy07 <40541235+Austy07@users.noreply.github.com>
2020-10-17 00:29:26 +0530

    header and footer ui done

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <main>
+            <Container>
+            <h1>Welcome to Shop</h1>
+            </Container>
+        </main>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
