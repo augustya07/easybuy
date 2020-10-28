a4411872deeaca3824d1f88de6062d20dbbf408c
Austy07 <40541235+Austy07@users.noreply.github.com>
2020-10-28 15:42:35 +0530

    Ready for Deployment

--- server/server.js
+++ server/server.js
@@ -29,17 +29,17 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
+    res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
@@ -53,8 +53,8 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
 )
